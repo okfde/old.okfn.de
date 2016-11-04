1d9553739a8391585487595f919b7282840ec86b
ffalt <ffalty@gmail.com>
2016-11-04 20:12:40 +0100

    use https://github.com/pattex/jekyll-tagging tag plugin

--- _plugins/tags.rb
+++ _plugins/tags.rb
@@ -1,37 +1,11 @@
+require 'jekyll/tagging'
+
 module Jekyll
-  class TagIndex < Page
-    def initialize(site, base, dir, tag)
-      #puts "----"
-      #puts dir
-      @site = site
-      @base = base
-      @dir = dir
-      @name = 'index.html'
-      self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
-      self.data['tag'] = tag
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Blog &gt; Tags &gt;'
-      tag_title_suffix = site.config['tag_title_suffix'] || ''
-      self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
-    end
-  end
-  class TagGenerator < Generator
-    safe true
-    def generate(site)
-      if site.layouts.key? 'tag_index'
-        dir = site.config['tag_dir'] || 'tag'
-        site.tags.keys.each do |tag|
-          if tag
-            write_tag_index(site, File.join(dir, tag.downcase), tag)
-          end
-        end
-      end
-    end
-    def write_tag_index(site, dir, tag)
-      index = TagIndex.new(site, site.source, dir, tag)
-      index.render(site.layouts, site.site_payload)
-      index.write(site.dest)
-      site.pages << index
+  module Tagify
+    def tagify(text)
+        text.to_s.replace_diacritics.downcase.gsub(/\s/, '-')
     end
   end
 end
+
+Liquid::Template.register_filter(Jekyll::Tagify)
