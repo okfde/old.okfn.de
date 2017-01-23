8cb247c90da189c0ad8faffbb4f020e84b46073f
ffalt <ffalty@gmail.com>
2017-01-23 15:14:46 +0100

    little translation fixes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
-gem install bundler
+#!/usr/bin/env sh
+
+# Make sure Bundler is installed
+if [ "$(gem query -i -n bundler)" = "false" ]; then
+  echo "Installing Bundler..."
+  gem install bundler
+fi
+
+# Set up Ruby dependencies via Bundler
+echo "Cleaning old Dependencies..."
 bundle clean --force
+
+# Set up Ruby dependencies via Bundler
+echo "Installing Dependencies..."
 bundle install --path .bundle/_vendor/bundle
